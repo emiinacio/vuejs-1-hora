58b140d98027edfe8bf334cad28344f281cd6a62
Emily Marcelino Inácio <55949281+emiinacio@users.noreply.github.com>
2022-02-06 19:42:53 +0000

    update project vueJS

--- vuejs-cli/src/components/Comments.vue
+++ vuejs-cli/src/components/Comments.vue
@@ -1,69 +1,55 @@
 <template>
+
     <div class="container">
         <h1>Comentários</h1>
-        <hr/>
-        <div class="form-todo form-group">
-            <p>
-                <input type="text" placeholder="nome" name="author" class="form-control" v-model="name">
-            </p>
-            <p>
-                <textarea name="message" placeholder="Comentário" class="form-control" v-model="message"></textarea>
-            </p>
-            <button v-on:click="addComment" type="submit" class="btn btn-primary">Comentar</button>
-        </div>
+        <hr />
+        <FormTodo v-on:add-todo="addComment"></FormTodo>
         <div class="list-group">
-            <div class="list-group-item" v-bind="(comment, index) in allComments">
-                <span class="comment_author">Autor: <strong>{{comment.name}}</strong></span>
-                <p>{{comment.message}}</p>
-                <div>
-                    <a href="#" title="Excluir" v-on:click.prevent="removeComment(index)">Excluir</a>
-                </div>
+        <p v-if="comments.length <= 0">Sem comentários...</p>
+        <div v-else class="list-group-item" v-for="(comment, index) in allComments" v-bind:key="index">
+            <span class="comment__author">Autor: <strong>{{ comment.name }}</strong></span>
+            <p>{{ comment.message }}</p>
+            <div>
+            <a href="#" title="Excluir" v-on:click.prevent="removeComment(index)">Excluir</a>
             </div>
         </div>
-        <hr/>
+        </div>
+        <hr />
     </div>
-</template>
+    </template>
 
-<script>
-export default {
+    <script>
+    import FormTodo from './FormTodo';
+    export default {
+    components: {
+        FormTodo
+    },
     data() {
-            return {
-                comments: [],
-                name: '',
-                message: ''
-            }
-        }, 
-        methods: {
-            addComment() {
-                if(this.message.trim() === ''){
-                    return;
-                }
-                this.comments.push({
-                    name: this.name,
-                    message: this.message
-                });
-
-                this.name = '',
-                this.message = ''
-            },
-            removeComment(index) {
-                this.comments.splice(index, 1);
-            }
-        },
-        computed: {
-            allComments(){
-                return this.comments.map(comment => ({
-                    ...comment,
-                    name: comment.name.trim() === '' ? 'Anônimo' : comment.name
-                }))
-            }
-        },
-        watch: {
-            comments() {
-                
-            }
+        return {
+        comments: []
         }
-};
+    },
+    methods: {
+        addComment(comment) {
+        this.comments.push(comment);
+        },
+        removeComment(index) {
+        this.comments.splice(index, 1);
+        }
+    },
+    computed: {
+        allComments() {
+        return this.comments.map(comment => ({
+            ...comment,
+            name: comment.name.trim() === '' ? 'Anônimo' : comment.name
+        }))
+        }
+    },
+    watch: {
+        comments(val) {
+        console.log('val', val)
+        }
+    }
+}
 </script>
-
             
